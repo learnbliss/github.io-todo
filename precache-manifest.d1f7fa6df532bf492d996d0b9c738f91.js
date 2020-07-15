97d4744d15054598be2b9279e25a8d7e1d778c75
Dmitriy Petrukhin <petrukhindmitriy@gmail.com>
2020-07-15 22:51:14 +0300

    Updates

---
+++ precache-manifest.d1f7fa6df532bf492d996d0b9c738f91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1808fe7448d2702ab09004435f109a2",
+    "url": "/github.io-todo/index.html"
+  },
+  {
+    "revision": "adf43226855997e58cb0",
+    "url": "/github.io-todo/static/css/main.0f339627.chunk.css"
+  },
+  {
+    "revision": "5ebec690c1c38b8fd209",
+    "url": "/github.io-todo/static/js/2.67a0f075.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/github.io-todo/static/js/2.67a0f075.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adf43226855997e58cb0",
+    "url": "/github.io-todo/static/js/main.f2550c41.chunk.js"
+  },
+  {
+    "revision": "0f96443055c9fe453f38",
+    "url": "/github.io-todo/static/js/runtime-main.250060ea.js"
+  }
+]);
